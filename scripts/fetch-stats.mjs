5b5ebe28bcfe3d49fbe0a2a2902b8f48fe6df276
mmshooreshi <mmshooreshi@gmail.com>
2025-05-10 01:06:40 +0330

    refactor(fetch): remove unused node-fetch import

--- scripts/fetch-stats.mjs
+++ scripts/fetch-stats.mjs
@@ -1,6 +1,5 @@
 // scripts/fetch-stats.mjs
 // import fs from "node:fs/promises";
-import fetch from "node-fetch";
 
 const fs = require("node:fs/promises");
 const users = ["HoseinM89", "MatinGG", "Reihaneh0-0", "Fuxgxugx135"];
